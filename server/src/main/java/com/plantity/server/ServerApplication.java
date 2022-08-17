293e08f116bdc60df78ed77ad69f1b7f5fc3c1a1
abi-hong <68041042+abi-hong@users.noreply.github.com>
2022-08-17 17:25:57 +0900

    [chore]#7 -  myplant DB들어가는지 postman으로 확인함

--- server/src/main/java/com/plantity/server/ServerApplication.java
+++ server/src/main/java/com/plantity/server/ServerApplication.java
@@ -1,5 +1,7 @@
 package com.plantity.server;
 
+import com.plantity.server.domain.myPlant.MyPlant;
+import com.plantity.server.domain.myPlant.MyPlantSaveRequestDto;
 import com.plantity.server.domain.users.Users;
 import com.plantity.server.repository.UsersRepository;
 import com.plantity.server.service.UsersService;
@@ -9,12 +11,46 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class ServerApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ServerApplication.class, args);
+
+		/*
+		try {
+			String plantName = "몬스테라";
+			String plantType = "식물타입";
+			int level = 1;
+			String content = "몬스테라는 식물입니다.";
+			String plantImage = "식물사진";
+			String status = "식물상태";
+
+			MyPlantSaveRequestDto requestDto = MyPlantSaveRequestDto.builder()
+					.plantName(plantName)
+					.plantType(plantType)
+					.level(level)
+					.content(content)
+					.plantImage(plantImage)
+					.status(status)
+					.build();
+		}
+		catch (Exception e) {
+			return;
+		}
+		finally {
+			return;
+		}
+		*/
+
+
 	}
 
 }
