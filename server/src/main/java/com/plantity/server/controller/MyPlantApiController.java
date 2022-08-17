b98290b368453e4a034728ac75bd995e8079b26a
abi-hong <68041042+abi-hong@users.noreply.github.com>
2022-08-17 17:51:19 +0900

    [chore] #7 - base response message 만들기

--- server/src/main/java/com/plantity/server/controller/MyPlantApiController.java
+++ server/src/main/java/com/plantity/server/controller/MyPlantApiController.java
@@ -5,17 +5,25 @@ import com.plantity.server.service.MyPlantService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/myplant")
 public class MyPlantApiController {
 
     private final MyPlantService myPlantService;
 
-    @PostMapping("/api/v1/myplants")
+    @PostMapping("/save")
     public Long save(@RequestBody MyPlantSaveRequestDto requestDto) {
 
-        return myPlantService.save(requestDto);
+        myPlantService.save(requestDto);
+
+        //성공 메세지
+
+        //에러 메세지
+
+        return;
     }
 }
