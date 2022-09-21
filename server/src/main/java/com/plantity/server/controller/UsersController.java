29e98fb3817a4295fcdd3eb169137e7a720fa34b
Jung Jein <o0o1910419@sookmyung.ac.kr>
2022-09-21 17:22:31 +0900

    [feat] #10 인가코드 추출 완료 + access 토큰으로 회원 아이디 접근 완료
    접근한 내용으로 회원가입 진행예정

--- server/src/main/java/com/plantity/server/controller/UsersController.java
+++ server/src/main/java/com/plantity/server/controller/UsersController.java
@@ -7,6 +7,7 @@ import com.plantity.server.domain.users.UsersResponseDto;
 import com.plantity.server.dto.res.users.UserResponse;
 import com.plantity.server.repository.UsersRepository;
 import com.plantity.server.service.UsersService;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,33 +17,50 @@ import java.util.List;
 import static com.plantity.server.constants.SuccessCode.USER_INFO_SUCCESS;
 
 @RestController
-@RequiredArgsConstructor
+@AllArgsConstructor
+//@RequiredArgsConstructor
 public class UsersController {
-    private final UsersRepository userRepository;
-    private final UsersService userService;
+//    private final UsersRepository userRepository;
+    private final UsersService usersService;
+//
+//    // user 목록 조회
+//    @GetMapping("/users")
+//    public List<Users> getUsers(){
+//        return userRepository.findAll();
+//    }
+//
+//    // user 상세 정보 조회
+//    @GetMapping("/users/{userId}")
+//    public ResponseEntity<UserResponse> getUser(@PathVariable Long userId) {
+//
+//        UsersRequestDto usersRequestDto = UsersRequestDto.of(userId);
+//
+//        UsersResponseDto usersResponseDto = userService.userInfo(usersRequestDto);
+//
+//        return UserResponse.newResponse(USER_INFO_SUCCESS, usersResponseDto);
+//    }
+//
+//    // 임시로 User 추가하기
+//    @PostMapping("/save/test/users")
+//    public BaseResponse2<Long> postTestUsers(@RequestBody UsersRequestDto usersRequestDto){
+//        Users users = new Users(usersRequestDto);
+//        userRepository.save(users);
+//        return new BaseResponse2<>(users.getUserId());
+//    }
 
-    // user 목록 조회
-    @GetMapping("/users")
-    public List<Users> getUsers(){
-        return userRepository.findAll();
-    }
+//    카카오 callback
+//    [GET] /oauth/kakao/callback
+//    code로 accessToken 추출하기
 
-    // user 상세 정보 조회
-    @GetMapping("/users/{userId}")
-    public ResponseEntity<UserResponse> getUser(@PathVariable Long userId) {
+    @ResponseBody
+    @GetMapping("/login/kakao") // access token 추출 확인 완
+    public void kakaoCallback(@RequestParam String code) {
+        //code 확인 완
+        System.out.println("controller code :" + code);
+        String access_Token = usersService.getKakaoAccessToken(code);
+        System.out.println("controller access_token :" + access_Token);
 
-        UsersRequestDto usersRequestDto = UsersRequestDto.of(userId);
-
-        UsersResponseDto usersResponseDto = userService.userInfo(usersRequestDto);
-
-        return UserResponse.newResponse(USER_INFO_SUCCESS, usersResponseDto);
-    }
-
-    // 임시로 User 추가하기
-    @PostMapping("/save/test/users")
-    public BaseResponse2<Long> postTestUsers(@RequestBody UsersRequestDto usersRequestDto){
-        Users users = new Users(usersRequestDto);
-        userRepository.save(users);
-        return new BaseResponse2<>(users.getUserId());
+        String userId = usersService.getUserId(access_Token);
+        System.out.println(userId);
     }
 }
